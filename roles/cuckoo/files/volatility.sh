0aa1d2e89f0371a10020d3563e1e5a5c66d6ae36
Unknown <frankmeeusen@gmail.com>
2017-04-11 11:27:43 +0200

    Preparing for next cuckoo version

--- roles/cuckoo/files/volatility.sh
+++
@@ -1,5 +0,0 @@
-curl -L https://github.com/volatilityfoundation/volatility/archive/2.4.1.tar.gz > /tmp/2.4.1.tar.gz
-cd /tmp
-tar -zxvf 2.4.1.tar.gz
-cd volatility-2.4.1
-python setup.py install
--- roles/cuckoo/files/yara.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-cd /tmp/yara
-touch foo
-echo whoami >> foo
-./bootstrap.sh
-./configure --enable-cuckoo --enable-magic
-make install
-cd yara-python && python setup.py build install && ldconfig
